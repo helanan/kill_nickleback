6cbc37f58cf3e9aedc42d4e2718b8c7549692a15
Helana <helanan@gmail.com>
2017-04-17 09:25:26 -0500

    Created nickleback.py

---
+++ nickleback.py
@@ -0,0 +1,4 @@
+songs = { ('Nickelback', 'How You Remind Me'), ('Will.i.am', 'That Power'), ('Miles Davis', 'Stella by Starlight'), ('Nickelback', 'Animals') } 
+# Using a set comprehension, create a new set that contains all songs that were not performed by Nickelback.
+
+nonNickelback = {}
